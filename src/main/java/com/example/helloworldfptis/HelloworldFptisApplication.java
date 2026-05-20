7b4f2e3fb4cf4821f64260f31f7172892d01273b
thong <AliasThog>
2026-05-20 09:48:32 +0700

    first commit

---
+++ src/main/java/com/example/helloworldfptis/HelloworldFptisApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldfptis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldFptisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldFptisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldfptis/HelloworldFptisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldfptis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldFptisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
